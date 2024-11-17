250696086d3c065df6c12e2ab4584b96e883f37b
Jayeoba Victor <victorjayeoba02@gmail.com>
2024-11-17 19:01:57 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+import React, { useState } from 'react';
 export default function App() {
   return (
-    <h1 className="text-5xl font-bold underline">
+    <h1 className="text-3xl bg-red-400 font-bold underline">
       Hello world!
     </h1>
   )
